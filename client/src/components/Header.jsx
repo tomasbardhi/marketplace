b0fc02eb9c4a4a175fbdfaedee9589608f41c7a6
tomasbardhi <tomasbardhi@gmail.com>
2021-11-23 09:28:51 +0100

    header + singlelist ui full responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,20 +2,29 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className="header">
-            <div className="logo">
-                <p>DeceNFT</p>
-            </div>
-            <div className="search">
-                <input type="search" name="" id="" />
-            </div>
-            <div className="menu">
-                <ul>
-                    <li>Explore</li>
-                    <li className="active">Stats</li>
-                    <li>Collections</li>
-                    <li>Wallet</li>
-                </ul>
+        <div className="headerWrapper">
+            <div className="header">
+                <div className="upperMenu">
+                    <div className="logo">
+                        <p>DeceNFT</p>
+                    </div>
+                    <div className="search">
+                        <input type="search" name="" id="" placeholder="Search NFTs, Collections and Creators" />
+                        <div className="searchIcon">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" /></svg>
+                        </div>
+                    </div>
+                </div>
+                <div className="lowerMenu">
+                    <div className="menu">
+                        <ul>
+                            <li>Explore</li>
+                            <li className="active">Stats</li>
+                            <li>Collections</li>
+                            <li>Wallet</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )
